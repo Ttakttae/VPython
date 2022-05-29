ba31055ec9ce67dc8f533b7daa632f173ba21438
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-05-29 14:40:25 +0900

    약간 이상한 태양계 완성, 겹치는 파일 삭제, 이름 이상한 폴더 변경

--- solar_system/solar_system.py
+++ solar_system/solar_system.py
@@ -1,40 +1,43 @@
 from vpython import *
 
+# scene.height = 800
+# scene.width = 1300
+
 
 sun=sphere(radius=18, color=color.red)
 mercury=sphere(radius=0.4, color=vec(153/255, 56/255, 0), pos=vec(19,0,0))
 venus=sphere(radius=0.9, color=color.yellow, pos= vec(21,0,0))
-earth=sphere(radius=1, color=color.cyan, pos=vec(25,0,0))
+earth=sphere(radius=1, texture=textures.earth, pos=vec(25,0,0))
 mars=sphere(radius=0.5, color=color.yellow, pos=vec(30,0,0))
 jupiter=sphere(radius=11.2, color=vec(153/255, 56/255, 0), pos=vec(50,0,0))
 saturn=sphere(radius=9.4, color=vec(242/255,150/255,97/255), pos=vec(80,0,0))
 uranos=sphere(radius=4, color=color.cyan, pos=vec(105,0,0))
+neptune=sphere(radius=4.2, color=vec(74/255, 108/255, 238/255), pos=vec(164, 0, 0))
 planets = []
-planets.append(mercury)
-planets.append(venus)
-planets.append(earth)
-planets.append(mars)
-planets.append(jupiter)
-planets.append(saturn)
-planets.append(uranos)
-
+planets.append([mercury, mercury.pos.x])
+planets.append([venus, venus.pos.x])
+planets.append([earth, earth.pos.x])
+planets.append([mars, mars.pos.x])
+planets.append([jupiter, jupiter.pos.x])
+planets.append([saturn, saturn.pos.x])
+planets.append([uranos, uranos.pos.x])
+planets.append([neptune, neptune.pos.x])
 
 
 while True:
   rate(50)
   for i in planets:
-    radius = i.pos.x
-    if i.pos.z >= 0:
-      if i.pos.x >= radius:
-        i.pos.x -= 0.1
-        i.pos.z = -(radius ** 2 - i.pos.x ** 2) ** 0.5
+    if i[0].pos.z >= 0:
+      if i[0].pos.x + 0.1 >= i[1]:
+        i[0].pos.x -= 0.1
+        i[0].pos.z = -abs((i[1] ** 2 - i[0].pos.x ** 2)) ** 0.5
       else:
-        i.pos.x += 0.1
-        i.pos.z = (radius ** 2 - i.pos.x ** 2) ** 0.5
+        i[0].pos.x += 0.1
+        i[0].pos.z = abs((i[1] ** 2 - i[0].pos.x ** 2)) ** 0.5
     else:
-      if i.pos.x <= -radius:
-        i.pos.x += 0.1
-        i.pos.z = (radius ** 2 - i.pos.x ** 2) ** 0.5
+      if i[0].pos.x - 0.1 <= -i[1]:
+        i[0].pos.x += 0.1
+        i[0].pos.z = abs((i[1] ** 2 - i[0].pos.x ** 2)) ** 0.5
       else:
-        i.pos.x -= 0.1
-        i.pos.z = -(radius ** 2 - i.pos.x ** 2) ** 0.5
+        i[0].pos.x -= 0.1
+        i[0].pos.z = -abs((i[1] ** 2 - i[0].pos.x ** 2)) ** 0.5
